12817376d8bab08b926d7b6cc10d400cdb9020b6
Stas <96624704+stabondar@users.noreply.github.com>
2025-07-08 11:48:01 +0300

    added calendar module

---
+++ src/modules/Calendar.js
@@ -0,0 +1,17 @@
+export default class Calendar
+{
+    constructor(instance, app)
+    {
+        this.instance = instance
+        this.app = app
+
+        this.destroyed = false
+
+        this.init()
+    }
+
+    init()
+    {
+
+    }
+}
